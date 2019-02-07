fc3786254879e2fb79a2135c8d82b8d9a87f215f
mwinata94 <mwinata@mail.sfsu.edu>
2019-02-07 15:36:54 -0800

    fix init warnings

--- UnitTest/FileReaderTest/TestInit.java
+++ UnitTest/FileReaderTest/TestInit.java
@@ -7,8 +7,8 @@ public class TestInit {
     private static final String FAIL_LOG = "Fail";
     public static void main( String[] args ) {
         try {
-            ProductReader productReader = new ProductReader();
-            TransactionReader transactionReader = new TransactionReader();
+            new ProductReader();
+            new TransactionReader();
             System.out.println(SUCCESS_LOG);
         } catch( Exception exception) {
             System.out.println(exception);
