7bb83f5f93adff6f577897088dc7bddeebf333eb
mwinata94 <mwinata@mail.sfsu.edu>
2019-02-08 07:41:31 -0800

    ProductParser methods are implemented and tested
    todo: refactor Catalog and Item classes

--- FileParser/ProductParser.java
+++ FileParser/ProductParser.java
@@ -1,7 +1,49 @@
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class ProductParser extends FileParser {
+    // private Object catalog;
+
     public ProductParser(String fileName) throws FileNotFoundException {
         super(fileName);
     }
+
+    public Object extractProducts() {
+        // catalog = new Object();
+        String testCase = "";
+        Object item;
+        while(null != (item = parseProduct())) {
+            // catalog.addItem(item);
+            testCase = testCase.concat((String)item);
+        }
+        // return catalog;
+        return testCase;
+    }
+
+    private Object parseProduct() {
+        try {
+            String id = parseUPC();
+            String name = parseDescription();
+            Double price = parsePrice();
+            // return new Item(id, name, price);
+            return String.format("id: %s\nname: %s\nprice: %.2f\n\n", id, name, price);
+        } catch(Exception exception) {
+            return null;
+        } 
+    }
+
+    private String parseUPC() throws IOException {
+        return parseSegment(4);
+    }
+
+    private String parseDescription() throws IOException {
+        parseSegment(5);
+        return parseSegment(20).trim();
+    }
+
+    private Double parsePrice() throws IOException {
+        parseSegment(5);
+        return Double.parseDouble(parseLine());
+    }
+
 }
---
+++ UnitTest/FileParserTest/TestProductParser.java
@@ -0,0 +1,24 @@
+import FileParser.*;
+
+public class TestProductParser {
+    private static final String PRODUCT_TEST_FILE_PATH = "./UnitTest/FileParserTest/productTest.txt";
+    private static final String TEST_CASE_OUTPUT = 
+        String.format("id: %s\nname: %s\nprice: %.2f\n\n", "1111", "Item 1", Double.parseDouble("15.20")) + 
+        String.format("id: %s\nname: %s\nprice: %.2f\n\n", "1112", "Item 2 jygkbytiby", Double.parseDouble("111111111")) +
+        String.format("id: %s\nname: %s\nprice: %.2f\n\n", "i8ny", "yw3b5w 463 46w 36pb4", Double.parseDouble("22.22"));
+    private static final String TEST_CASE_FAIL_LOG = "product parsing failed";
+    private static final String SUCCESS_LOG = "Success";
+    private static final String FAIL_LOG = "Fail";
+    public static void main( String[] args ) {
+        try {
+            ProductParser testProductParser = new ProductParser(PRODUCT_TEST_FILE_PATH);
+            if (!TEST_CASE_OUTPUT.equals(testProductParser.extractProducts())) {
+                throw new Exception(TEST_CASE_FAIL_LOG);
+            }
+            System.out.println(SUCCESS_LOG);
+        } catch( Exception exception) {
+            System.out.println(exception);
+            System.out.println(FAIL_LOG);
+        }
+    }
+}
