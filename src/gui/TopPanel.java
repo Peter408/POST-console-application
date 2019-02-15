895d00b0d25bf539dc6fcd0819b8c39b783c5dd1
mwinata94 <mwinata@mail.sfsu.edu>
2019-02-15 15:16:07 -0800

    implement TopPanel label and text field components

--- src/gui/TopPanel.java
+++ src/gui/TopPanel.java
@@ -1,4 +1,31 @@
 package gui;
 
-public class TopPanel {
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import javax.swing.JPanel;
+import java.awt.Label;
+import java.awt.TextField;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+
+public class TopPanel extends JPanel {
+    static final long serialVersionUID = 1001;
+    private TextField customerName = new TextField();
+
+    public TopPanel() {
+        this.setLayout(new GridBagLayout());
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        this.add(new Label("Customer"), gridBagConstraints);
+        gridBagConstraints.gridy = 1;
+        this.add(customerName, gridBagConstraints);
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridx = 1;
+        this.add(new Label((new SimpleDateFormat("MMM dd, yyyy")).format(new Date())), gridBagConstraints);
+    }
+
+    public String getCustomerName() {
+        return customerName.getText();
+    }
 }
