b4855464916293c796ff7c002ca589e8fa45ce10
mwinata94 <mwinata@mail.sfsu.edu>
2019-02-15 16:29:22 -0800

    set topPanel layout

--- src/gui/TopPanel.java
+++ src/gui/TopPanel.java
@@ -1,17 +1,18 @@
 package gui;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Label;
-import java.awt.TextField;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class TopPanel extends JPanel {
     static final long serialVersionUID = 1001;
-    private TextField customerName = new TextField();
+    private JTextField customerName = new JTextField();
     private static final int MAX_WIDTH = 800;
     private static final int MAX_HEIGHT = 75;
     private static final int ITEM_GAP = 5;
@@ -19,22 +20,47 @@ public class TopPanel extends JPanel {
     private Dimension dimension;
 
     public TopPanel() {
-        this.dimension = new Dimension(MAX_WIDTH, MAX_HEIGHT);
-        this.setPreferredSize(dimension);
-        this.setSize(dimension);
-        this.setLayout(new GridBagLayout());
-        GridBagConstraints gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 0;
-        this.add(new Label("Customer"), gridBagConstraints);
-        gridBagConstraints.gridy = 1;
-        this.add(customerName, gridBagConstraints);
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.gridx = 1;
-        this.add(new Label((new SimpleDateFormat("MMM dd, yyyy")).format(new Date())), gridBagConstraints);
+        setDefaultConfiguration();
+        setComponents();
     }
 
     public String getCustomerName() {
         return customerName.getText();
+    }
+
+    private void setDefaultConfiguration() {
+        this.dimension = new Dimension(MAX_WIDTH, MAX_HEIGHT);
+        this.setPreferredSize(dimension);
+        this.setSize(dimension);
+        this.setLayout(new BorderLayout());
+    }
+
+    private void setComponents(){
+        setLeftComponents();
+        setRightComponents();
+    }
+
+    private void setLeftComponents() {
+        JPanel gridBag = new JPanel();
+        gridBag.setLayout(new GridBagLayout());
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
+        gridBag.add(new Label("Customer"), gridBagConstraints);
+        gridBagConstraints.gridy = 1;
+        customerName.setColumns(20);
+        gridBag.add(customerName, gridBagConstraints);
+        this.add(gridBag, BorderLayout.WEST);
+    }
+
+    private void setRightComponents() {
+        JPanel gridBag = new JPanel();
+        gridBag.setLayout(new GridBagLayout());
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridx = 0;
+        gridBag.add(new Label((new SimpleDateFormat("MMM dd, yyyy")).format(new Date())), gridBagConstraints);
+        this.add(gridBag, BorderLayout.EAST);
     }
 }
