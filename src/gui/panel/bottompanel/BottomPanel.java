7883f45ad3c20e28d5f0f559596a9cdd0ef2b2f0
Peter Le <peterle408@gmail.com>
2019-02-15 20:09:41 -0800

    add radio buttom WIP

--- src/gui/panel/bottompanel/BottomPanel.java
+++ src/gui/panel/bottompanel/BottomPanel.java
@@ -3,11 +3,9 @@ package gui.panel.bottompanel;
 import javax.swing.*;
 import java.awt.*;
 
-public class BottomPanel extends  JPanel{
+public class BottomPanel extends  JPanel {
     private static final int MAX_WIDTH = 800;
     private static final int MAX_HEIGHT = 125;
-    private static final int TEXT_HEIGHT = 30;
-    private static final int FONT_SIZE = 14;
 
     private Dimension dimension;
 
@@ -15,13 +13,12 @@ public class BottomPanel extends  JPanel{
         this.dimension = new Dimension( MAX_WIDTH, MAX_HEIGHT);
         this.setPreferredSize( dimension );
         this.setSize( dimension );
-        this.setBackground(Color.LIGHTBLUE);
+        this.setBackground(Color.BLUE);
 
         this.setLayout(new BorderLayout());
-
-    }
-
-    private static JFrame createPaymentTypePanel() {
-        
+        // add payenttype
+        this.add(PaymentType.createPaymentType(), BorderLayout.NORTH);
+        // add total
+        // add checkout
     }
 }
---
+++ src/gui/panel/bottompanel/PaymentType.java
@@ -0,0 +1,46 @@
+package gui.panel.bottompanel;
+
+import javax.swing.*;
+
+import com.sun.org.apache.xalan.internal.templates.Constants;
+
+import java.awt.*;
+
+public class PaymentType {
+    private static final int MAX_WIDTH = 400; // change this shit cuz im stupid
+    private static final int TEXT_HEIGHT = 30;
+    
+    static JPanel createPaymentType() {
+        GridBagConstraints constraints = setConstraints();
+
+        JPanel paymentPanel = new JPanel();
+        paymentPanel.setLayout(new GridBagLayout());
+        
+        // check
+        // or make as JPanal and add to it from the function
+        JRadioButton checkPanel = createRadioButton("Check");
+        paymentPanel.add(checkPanel, constraints);
+        // cash
+        // credit
+        
+        return paymentPanel;
+    }
+
+    private static GridBagConstraints setConstraints() {
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 1;
+        constraints.anchor = GridBagConstraints.NORTHWEST;
+        return constraints;
+    }
+
+    private static JRadioButton createRadioButton(String name) {
+        JRadioButton radioButton = new JRadioButton();
+        radioButton.setText(name);
+        // set bound ?
+        // radioButton.setBounds(0, 25, 25, 25); 
+        return radioButton;
+    }
+}
