725b4fb77dace7f07bb17bd19bde65f64b28d6a7
Peter Le <peterle408@gmail.com>
2019-02-15 17:47:45 -0800

    initial bottom panel

--- src/gui/BottomPanel.java
+++ src/gui/BottomPanel.java
@@ -6,13 +6,22 @@ import java.awt.*;
 public class BottomPanel extends  JPanel{
     private static final int MAX_WIDTH = 800;
     private static final int MAX_HEIGHT = 125;
+    private static final int TEXT_HEIGHT = 30;
+    private static final int FONT_SIZE = 14;
 
     private Dimension dimension;
 
-    public BottomPanel(){
+    public BottomPanel() {
         this.dimension = new Dimension( MAX_WIDTH, MAX_HEIGHT);
         this.setPreferredSize( dimension );
         this.setSize( dimension );
-        this.setBackground(Color.BLUE);
+        this.setBackground(Color.LIGHTBLUE);
+
+        this.setLayout(new BorderLayout());
+
+    }
+
+    private static JFrame createPaymentTypePanel() {
+        
     }
 }
