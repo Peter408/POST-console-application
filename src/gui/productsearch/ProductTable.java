047f58a05fa7c80afa0c0514f96c32515bb01ac9
mwinata94 <mwinata@mail.sfsu.edu>
2019-02-17 17:06:49 -0800

    add table to product table

--- src/gui/productsearch/ProductTable.java
+++ src/gui/productsearch/ProductTable.java
@@ -1,27 +1,81 @@
 package gui.productsearch;
 
-import store.Catalog;
+import item.Item;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Vector;
 
 import javax.swing.*;
-import java.awt.*;
+import javax.swing.table.DefaultTableModel;
 
 public class ProductTable extends JPanel {
+
+    static final long serialVersionUID = 60001;
     private static final int WIDTH = 600;
     private static final int HEIGHT = 300;
+    private static final int SECTION_WIDTH = WIDTH / 37;
+    private static final int COLUMN_COUNT = 4;
+    private static final String[] COLUMN_NAMES = {
+        "UPC",
+        "Item",
+        "QTY",
+        "Unit Price"
+    };
+    private static final int[] COLUMN_WIDTHS = {
+        SECTION_WIDTH * 4,
+        SECTION_WIDTH * 20,
+        SECTION_WIDTH * 3,
+        SECTION_WIDTH * 10
+    };
     private Dimension dimension;
+    private DefaultTableModel tableModel;
+    private JTable table;
+    private ArrayList<Item> data;
 
-    Catalog catalog;
-
-    ProductTable(Catalog catalog) {
-        this.catalog = catalog;
-        initialize();
+    public ProductTable() {
+        setDefaultConfiguration();
+        setComponents();
     }
 
-    public void initialize() {
+    private void setDefaultConfiguration() {
         this.dimension = new Dimension(WIDTH, HEIGHT);
         this.setPreferredSize(dimension);
         this.setSize(dimension);
         this.setBackground(Color.CYAN);
+        this.setLayout(new BorderLayout());
+        this.data = new ArrayList<>();
+        tableModel = new ProductTableModel(new Vector<String>(Arrays.asList(COLUMN_NAMES)), 0);
+        table = new JTable(tableModel);
+        table.setFillsViewportHeight(true);
+        for (int i = 0; i < COLUMN_COUNT; i++) {
+            table.getColumnModel().getColumn(i).setPreferredWidth(COLUMN_WIDTHS[i]);    
+        }
+    }
+
+    public void setComponents() {
+        JScrollPane scrollPane = new JScrollPane(table);
+        add(scrollPane, BorderLayout.PAGE_START);
+    }
+
+    public void setData(ArrayList<Item> data) {
+        /* TODO */
+    }
+
+    class ProductTableModel extends DefaultTableModel {
+
+        static final long serialVersionUID = 60001;
+    
+        public ProductTableModel(Vector<String> v, int row) {
+            super(v, row);
+        }
+
+        @Override
+        public boolean isCellEditable(int row, int column) {
+            return false;
+        }
+
     }
 
 }
