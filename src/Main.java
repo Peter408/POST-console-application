0ce77505cbc4c43122f35f42f673b1e01d612f16
Eric Groom <egroom@mail.sfsu.edu>
2019-02-17 16:57:48 -0800

    hotfix for POST class name

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ import post.POST;
 
 public class Main {
     public static void main(String[] args) {
-        Post post = new POST("https://post-server.herokuapp.com");
-        new ShopFrame( post );
+        POST post = new POST("https://post-server.herokuapp.com");
+        new ShopFrame(post);
         new AddItemFrame(post.getCatalog());
     }
 }
